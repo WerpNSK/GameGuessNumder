2e8379b7fb387c464d5e5d960c1555dde8bc713c
WerpNSK <mininnick@yandex.ru>
2020-12-24 16:07:12 +0700

    Добавьте файлы проекта.

---
+++ GameGuessNumder/guessNuberGame.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void main()
+{
+	setlocale(0, "");
+	int num;
+	
+	srand(time(0));
+	//орандом от 1 до 10
+	int random = rand() % 10 + 1;
+	
+		
+	while (num == random && num ==0)
+	{
+		
+	}
+
+
+	system("pause");
+}
